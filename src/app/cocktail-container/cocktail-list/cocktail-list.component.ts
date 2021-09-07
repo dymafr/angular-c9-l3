9a3e49a89dbd373a0e34eb78000261cc98627045
dymafr <erwan1309@hotmail.com>
2021-09-07 13:58:30 +0200

    3 files modified

--- src/app/cocktail-container/cocktail-list/cocktail-list.component.ts
+++ src/app/cocktail-container/cocktail-list/cocktail-list.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
-import { Cocktail } from "../../shared/interfaces/cocktail.interface";
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Cocktail } from '../../shared/interfaces/cocktail.interface';
 
 @Component({
-  selector: "app-cocktail-list",
-  templateUrl: "./cocktail-list.component.html",
-  styleUrls: ["./cocktail-list.component.scss"]
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.scss']
 })
 export class CocktailListComponent implements OnInit {
-  @Input() public cocktails: Cocktail[];
+  @Input() public cocktails?: Cocktail[];
   @Output() private changeCocktail: EventEmitter<number> = new EventEmitter<
     number
   >();
